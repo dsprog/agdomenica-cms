6452ba74ed2fdcab13e1086b68a5e317e2759107
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2023-01-25 13:15:40 -0300

    ajuste page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,10 +4,6 @@
     </div>
     <div class="container mx-auto py-4 md:py-6">
         <p><strong>Links úteis</strong><br>
-            <a href="{{route('site.sobre')}}">&bull; Conheça a cegea</a><br>
-            <a href="{{route('site.posgraduacao')}}">&bull; Confira nosso curso de pós-graduação</a><br>
-            <a href="{{route('site.cursos')}}">&bull; Confira nossos cursos</a><br>
-            <a href="{{route('site.contato')}}">&bull; Entre em contato conosco</a><br>
         </p>
     </div>
 </x-app-layout>
