5f7925057aefbc0ce914241e43294fa30f354d63
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2023-01-26 01:59:06 -0300

    topo, rodape e inicio da home

--- resources/views/site-footer.blade.php
+++ resources/views/site-footer.blade.php
@@ -1,37 +1,34 @@
-<footer>
+<footer class="rodape py-2 md:py-10">
     <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-8 px-4 py-4">
-            <div class="col-span-3 pb-4 md:pb-0">
-                <div class="endereco text-white text-sm">
-                    {{ config('configs.address') }}, {{ config('configs.address_number') }} -
-                    {{ config('configs.address_city') }}/{{ config('configs.address_state') }} -
-                    {{ config('configs.address_cep') }}<br>
-                    @if (config('configs.address_complement'))
-                        {{ config('configs.address_complement') }}<br>
-                    @endif
-                    Escola Superior de Agricultura "Luiz de Queiroz"<br>
-                    Fone: {{ config('configs.phone') }}
-                    @if (config('configs.phone2'))
-                     / {{ config('configs.phone2') }}
-                    @endif
-                    <br>WhatsApp: {{ config('configs.whatsapp') }}
-                </div>
+        <h4 class="text-white text-3xl text-center font-bold pb-5">CONTATO</h4>
+        <div class="grid grid-cols-1 md:grid-cols-6 px-4 py-4">
+            <div class="col-span-2">
+                <p class="text-white text-lg font-semibold pl-4">ANTONIO R. COELHO NETO<br>
+                    <a href="http://wa.me/5519998896111" target="_blank">55 19 99889-6111</a><br>
+                    <a href="mailto:neto@agdomenica.com.br" class="text-blue-400">neto@agdomenica.com.br</a></p>
+
+                <p class="text-white text-lg font-semibold pl-4 pt-6">LEA FABRIS<br>
+                    <a href="http://wa.me/5519998898661" target="_blank">55 19 99889-8661</a><br>
+                    <a href="mailto:lea@agdomenica.com.br" class="text-blue-400">lea@agdomenica.com.br</a></p>
             </div>
-            <div class="col-span-1 hidden md:block md:pb-0">
-                <a href="{{ route('site.contato') }}" class="text-white text-sm">:: Contato</a><br>
+            <div class="col-span-4">
+                <form action="#">
+                    <input type="text" name="nome" id="nome" placeholder="NOME"
+                        class="bg-white text-gray-900 w-full mb-2">
+                    <input type="text" name="email" id="email" placeholder="E-MAIL"
+                        class="bg-white text-gray-900 w-full mb-2">
+                    <input type="text" name="telefone" id="telefone" placeholder="TELEFONE"
+                        class="bg-white text-gray-900 w-full mb-2">
+                    <input type="text" name="assunto" id="assunto" placeholder="ASSUNTO"
+                        class="bg-white text-gray-900 w-full mb-2">
+                    <textarea name="mensagem" id="mensagem" placeholder="MENSAGEM" cols="30" rows="5"
+                        class="bg-white text-gray-900 w-full mb-2"></textarea>
+                    <div class="text-right">
+                    <input type="submit" value="ENVIAR" class="bg-gray-400 text-gray-900 font-bold py-3 px-5 float-right"></div>
+                </form>
             </div>
-            <div class="col-span-1 hidden md:block md:pb-0">
-                <a href="{{ route('site.index') }}" class="text-white text-sm">:: Home</a><br>
-            </div>
-            <div class="col-span-3 pb-4 md:pb-0">
-                <p class="text-center lg:text-right"><img src="{{ asset("site/apoio.png")}}"
-                    alt="Apoio: USP, Esalq e Fealq" class="inline-block"></p>
-            </div>
-        </div>
-        <div class="grid md:grid-cols-2 px-4 py-4">
-            <div class="text-white text-sm text-center md:text-left">CEGEA - Todos os direitos reservados - {{ now()->year }}</div>
-            <div class="text-white text-sm text-center md:text-right">Desenvolvido por:
-                <a href="https://cubolibre.com.br" target="_blank" class="text-gray-300">CuboLibre</a></div>
         </div>
     </div>
 </footer>
+<p class="text-center py-3 text-sm">Projeto gráfico Domenica Publicidade Ltda. |
+    Programação <a href="https://dsprog.com.br" target="_blank">Dsprog</a> | Todos os direitos reservados</p>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
     <!--[if (lt IE 9)]><script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/min/tiny-slider.helper.ie8.js"></script><![endif]-->
     @endpush
-    <div class="container mx-auto text-center bg-gray-200">
+    <div class="w-full text-center bg-gray-200">
         <div class="banner-home">
             @foreach ($banners as $banner)
             <div><a href="{{ $banner->url }}">
@@ -13,6 +13,40 @@
             @endforeach
         </div>
     </div>
+    <main class="py-4 md:py-10">
+        <div class="container mx-auto">
+            <h2 class="text-center text-3xl font-bold text-gray-500 uppercase py-10">Clientes</h2>
+        </div>
+        <p><img src="{{asset('site/neto-lea.jpg')}}" alt="Foto da Lea e Neto" class="w-full pb-10"></p>
+        <div class="container mx-auto">
+            <h2 class="text-center text-3xl font-bold text-gray-500 uppercase py-5">Quem Somos</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 py-4">
+                <div>
+                    <p class="pb-3">Mais do que ser uma agência de publicidade, somos apaixonados pelo nosso trabalho.<br>
+                        Em mais de três décadas de mercado, passamos por todas as fases da comunicação, desde os bons
+                        tempos dos veículos de mídias impressas, as estratégias e campanhas de varejo, o planejamento
+                        estratégico e as produções, para um universo bastante restrito em relação ao atual.<br>
+                        Grandes lutas para que o mercado regional entendesse a função de uma agência, o desenvolvimento
+                        de colaboradores, parceiros e fornecedores, eventos, feiras e muito mais.</p>
+
+                    <p class="pb-3">Toda essa trajetória nos possibilitou uma grande base de conhecimento, das criações às
+                        produções, trazendo vasta expertise para que hoje, a <strong>Domenica Publicidade</strong>
+                        possa atuar com muito mais conteúdo, no novo universo digital e suas vertentes.</p>
+                </div>
+                <div>
+                    <p class="pb-3">Falar da comunicação de uma empresa, é falar sobre sua identidade, seu DNA, seus players e,
+                        em especial, traduzir os seus valores.<br>
+                        Ser apenas uma agência é muito pouco para se definir a importância do nosso trabalho.</p>
+
+                    <p class="pb-3">Muito mais que criações, desenvolvemos estratégias para uma comunicação versátil,
+                        inteligente e economicamente viável aos clientes.</p>
+                    <p class="pb-3">Deixamos aqui um convite para que possamos conhecer melhor a sua empresa e lhe entregar o
+                        nosso melhor:<br>
+                        a nossa paixão!</p>
+                </div>
+            </div>
+        </div>
+    </main>
     @push('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
     <!-- NOTE: prior to v2.2.1 tiny-slider.js need to be in <body> -->
